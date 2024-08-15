adbbf2ce06f716dfc422ed887866aebf1dae5b6e
Meem Fatema <meemfatema95@gmail.com>
2024-08-15 15:53:12 -0400

    create login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import { signInWithPopup } from 'firebase/auth';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Login = () => {
+    const {signIn,googleSignIn}=useContext(AuthContext)
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        const form=e.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        signIn(email,password)
+        console.log(email, password)
+
+    }
+    const handleGoogleSignIn = () =>{
+        googleSignIn()
+    }
     return (
         <div>
-            login page
+              <div>
+      <div className="my-8">
+      <h1 className="text-4xl font-bold text-center">Login</h1></div>
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-100  shadow-2xl text-black mx-auto mb-14">
+       
+        <form onSubmit={handleLogin} noValidate="" action="" className="space-y-6">
+          <div className="space-y-1 text-sm">
+            <label htmlFor="email" className="block text-black">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Username"
+              className="w-full px-4 py-3 rounded-md border-gray-700 bg-white text-black focus:border-violet-400"
+            />
+          </div>
+
+          <div className="space-y-1 text-sm">
+            <label htmlFor="password" className="block text-black">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="w-full px-4 py-3 rounded-md border-gray-700 bg-white text-black focus:border-violet-400"
+            />
+          </div>
+          <button className="block w-full p-3 text-center rounded-sm text-white text-lg bg-lime-600">
+            Login
+          </button>
+        </form>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
+          <p className="px-3 text-sm text-black">Login with social accounts</p>
+          <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
+        </div>
+        <div className="flex justify-center space-x-4">
+          <button onClick={handleGoogleSignIn} aria-label="Log in with Google" className="p-3 rounded-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              className="w-5 h-5 fill-current"
+            >
+              <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+            </svg>
+          </button>
+ 
+         
+        </div>
+        <p className="text-xs text-center sm:px-6 text-black">
+          Do not have an account?
+          <Link to="/register">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="underline text-black"
+            >
+              {" "}
+            Sign up
+          </a>
+          </Link>
+
+        </p>
+      </div>
+    </div>
         </div>
     );
 };
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SignUp = () => {
     
-    newuser(a,b)
+    //newuser(a,b)
     return (
         <div>
             signup
